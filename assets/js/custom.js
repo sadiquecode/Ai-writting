62189cae7951c45145a7c965f1c3a3cd21c1c39b
Younas Mahmood <muhammadsadique116@gmail.com>
2023-07-06 22:56:52 +0500

    aiwritting

---
+++ assets/js/custom.js
@@ -0,0 +1,76 @@
+var dropdownToggle = document.querySelector(".custom-dropdown-toggle");
+dropdownToggle.addEventListener("click", function () {
+  var dropdownMenu = this.nextElementSibling;
+  dropdownMenu.style.display =
+    dropdownMenu.style.display === "block" ? "none" : "block";
+});
+
+$(".slider_images").slick({
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  autoplay: true,
+});
+
+var filtered = false;
+
+$(".js-filter").on("click", function () {
+  if (filtered === false) {
+    $(".filtering").slick("slickFilter", ":even");
+    $(this).text("Unfilter Slides");
+    filtered = true;
+  } else {
+    $(".filtering").slick("slickUnfilter");
+    $(this).text("Filter Slides");
+    filtered = false;
+  }
+});
+
+$(".slicktwo").slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
+        autoplay: true,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+      },
+    },
+  ],
+});
+// bottom scroll button
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $("#scrollToTopBtn").fadeIn();
+    } else {
+      $("#scrollToTopBtn").fadeOut();
+    }
+  });
+  $("#scrollToTopBtn").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, "slow");
+  });
+});
